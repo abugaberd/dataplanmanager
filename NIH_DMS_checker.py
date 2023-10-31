b68ac6f508d5e929e00b8285bcb3266af467e340
David Abugaber <116297881+abugaberd@users.noreply.github.com>
2023-10-31 09:13:47 -0400

    Update NIH_DMS_checker.py

--- NIH_DMS_checker.py
+++ NIH_DMS_checker.py
@@ -45,19 +45,20 @@ def run():
     if input_method == 'Paste in text':
         dms_text = st.text_input("Paste the text of your Data Management & Sharing plan to check")
     
-    with st.sidebar:
-      st.title('Customize Model Parameters')
-      if 'REPLICATE_API_TOKEN' in st.secrets:
-            st.success('API key already provided!', icon='✅')
-            replicate_api = st.secrets['REPLICATE_API_TOKEN']
-      else:
-            replicate_api = st.text_input('Enter Replicate API token:', type='password')
-            if not (replicate_api.startswith('r8_') and len(replicate_api)==40):
-                st.warning('Please enter your credentials!', icon='⚠️')
-            else:
-                st.success('Proceed to entering your prompt message!', icon='👉')
-      os.environ['REPLICATE_API_TOKEN'] = replicate_api
+    #with st.sidebar:
+      #st.title('Customize Model Parameters')
+      #if 'REPLICATE_API_TOKEN' in st.secrets:
+      #      st.success('API key already provided!', icon='✅')
+      #      replicate_api = st.secrets['REPLICATE_API_TOKEN']
+      #else:
+      #      replicate_api = st.text_input('Enter Replicate API token:', type='password')
+      #      if not (replicate_api.startswith('r8_') and len(replicate_api)==40):
+      #          st.warning('Please enter your credentials!', icon='⚠️')
+      #      else:
+      #          st.success('Proceed to entering your prompt message!', icon='👉')
+    os.environ['REPLICATE_API_TOKEN'] = st.secrets['REPLICATE_API_TOKEN']
 
+    with st.sidebar:
       st.subheader('Models and parameters')
       selected_model = st.sidebar.selectbox('Choose a Llama2 model', ['Llama2-7B', 'Llama2-13B'], key='selected_model')
       if selected_model == 'Llama2-7B':
